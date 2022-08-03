241bb7fcd47b9ecdc7488bc476a74ea0f947cd30
anoopjoy999 <65029416+anoopjoy999@users.noreply.github.com>
2022-08-03 15:22:32 +0100

    Update NaceService.java
    removing error

--- src/main/java/com/example/demo/service/NaceService.java
+++ src/main/java/com/example/demo/service/NaceService.java
@@ -1,8 +1,5 @@
 package com.example.demo.service;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.example.demo.entity.NaceOrder;
 import com.example.demo.exceptions.NaceException;
 import com.example.demo.model.NaceOrderDto;
@@ -10,7 +7,7 @@ import com.example.demo.model.NaceOrderDto;
 public interface NaceService {
 	
 	
-	public NaceOrder putNaceDetails(@RequestBody NaceOrderDto user) throws NaceException;
-	public NaceOrderDto getNaceDetails(@RequestParam String order) throws NaceException;
+	public NaceOrder putNaceDetails(NaceOrderDto user) throws NaceException;
+	public NaceOrderDto getNaceDetails(String order) throws NaceException;
 
 }
